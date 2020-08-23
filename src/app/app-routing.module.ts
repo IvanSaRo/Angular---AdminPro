bb56f51f373ef79be9c1ec87a1edb5c3e69da653
Iván Sánchez <ivansancrodr@gmail.com>
2020-08-24 00:19:22 +0200

    Rutas auth en auth.routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,30 +2,33 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PagesRoutingModule } from './pages/pages.routing';
-
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-
-
+import { AuthRoutingModule } from './auth/auth.routing';
 
 import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
 
-
 const routes: Routes = [
- 
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '**', component: NopagesfoundComponent}
+ /*  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ],
+  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }, */
+  { path: '**', component: NopagesfoundComponent },
 ];
-
-
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot( routes ),
-    PagesRoutingModule
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule,
   ],
-  exports: [ RouterModule ]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
