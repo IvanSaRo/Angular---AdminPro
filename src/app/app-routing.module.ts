618503521037ea3551fbb4d92de09b94966b6db2
Iván Sánchez <ivansancrodr@gmail.com>
2020-08-23 23:53:30 +0200

    App module limpiado. Auth, pages y shared modulos propios. Nopagesfound movido a raiz para que accedan tanto usuarios como invitados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule, Component } from '@angular/core';
-import { Router, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
 import { PagesComponent } from './pages/pages.component';
+
+import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
 
 
 const routes: Routes = [
