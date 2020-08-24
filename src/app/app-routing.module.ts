b0e1abdc00d1f2cd8bd9c8c4840265cdfd24a10c
Iván Sánchez <ivansancrodr@gmail.com>
2020-08-24 16:19:56 +0200

    Estructura html y css progress implementada, barra de progreso y botones añadidos,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,12 @@ import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
 
 const routes: Routes = [
  /*  {
-    path: '',
+    path: 'dashboard',
     component: PagesComponent,
     children: [
-      { path: 'dashboard', component: DashboardComponent },
+      { path: '', component: DashboardComponent },
       { path: 'progress', component: ProgressComponent },
       { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     ],
   },
   { path: 'register', component: RegisterComponent },
