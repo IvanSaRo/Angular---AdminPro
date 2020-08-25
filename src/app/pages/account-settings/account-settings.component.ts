f6710c85be2373047d496d1f02e7614cf33cd2df
Iván Sánchez <ivansancrodr@gmail.com>
2020-08-25 16:41:29 +0200

    reubicada la lógica de account-settings en un servicio para limpiar el componente

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -8,16 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  //saco estas dos declaraciones de sus funciones para no acceder al DOM cada vez que se activan las finciones, lo cual gasta innecesariamente recursos
-  public linkTheme = document.querySelector('#theme');
-
-  public links:  NodeListOf<Element>; //he de indicar los valores en ngOnInit ya que en ese momento ya se ha inicializado el componente y puede barrer los colores porque aún no se ha construido el html
   
-  constructor() { }
+
+   //he de indicar los valores en ngOnInit ya que en ese momento ya se ha inicializado el componente y puede barrer los colores porque aún no se ha construido el html
+  
+  constructor( private settingsService: SettingsService) { }
 
   ngOnInit() {
-    this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTheme();
+    
+    this.settingsService.checkCurrentTheme();
 
   }
 
@@ -25,36 +25,8 @@ export class AccountSettingsComponent implements OnInit {
 
   changeTheme( theme: string){
 
-   const url = `./assets/css/colors/${ theme }.css`;
-   
-
-   this.linkTheme.setAttribute('href', url);
-
-   localStorage.setItem('theme', url);
-
-   this.checkCurrentTheme();
+   this.settingsService.changeTheme( theme );
   }
 
-  checkCurrentTheme(){
-    
-
-    this.links.forEach( elem => {
-
-      elem.classList.remove('working');
-
-      const btnTheme = elem.getAttribute('data-theme');
-
-      const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
-
-      const currentTheme = this.linkTheme.getAttribute('href');
-
-      if( btnThemeUrl === currentTheme ){
-
-        elem.classList.add('working');
-
-      }
-    });
-
-
-  }
+  
 }
