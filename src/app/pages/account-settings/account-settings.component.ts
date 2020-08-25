d0e7c595868b83a99b540fc2d142c25f64bf4e20
Iván Sánchez <ivansancrodr@gmail.com>
2020-08-25 15:54:23 +0200

    añadido tick en selección tema en account settings

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,12 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
+  //saco estas dos declaraciones de sus funciones para no acceder al DOM cada vez que se activan las finciones, lo cual gasta innecesariamente recursos
   public linkTheme = document.querySelector('#theme');
+
+  public links:  NodeListOf<Element>; //he de indicar los valores en ngOnInit ya que en ese momento ya se ha inicializado el componente y puede barrer los colores porque aún no se ha construido el html
   
   constructor() { }
 
-  ngOnInit(): void {
-
+  ngOnInit() {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
 
   }
 
@@ -21,11 +25,36 @@ export class AccountSettingsComponent implements OnInit {
 
   changeTheme( theme: string){
 
-   const url = `./assets/css/colors/${ theme }.css`
+   const url = `./assets/css/colors/${ theme }.css`;
    
 
    this.linkTheme.setAttribute('href', url);
 
    localStorage.setItem('theme', url);
+
+   this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme(){
+    
+
+    this.links.forEach( elem => {
+
+      elem.classList.remove('working');
+
+      const btnTheme = elem.getAttribute('data-theme');
+
+      const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
+
+      const currentTheme = this.linkTheme.getAttribute('href');
+
+      if( btnThemeUrl === currentTheme ){
+
+        elem.classList.add('working');
+
+      }
+    });
+
+
   }
 }
