a6dd3c4427f417902e8b466a289fbd1b1fd5e9ab
Iván Sánchez <ivansancrodr@gmail.com>
2020-09-12 18:46:44 +0200

    validación repetir password en registro ok

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,11 +12,13 @@ export class RegisterComponent {
   
   
   public registerForm = this.fb.group({
-    name: ['', [ Validators.required, Validators.minLength(3)]],
-    email: ['', [Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),Validators.required]],
+    name: ['iván', [ Validators.required, Validators.minLength(3)]],
+    email: ['ivan@gmail.com', [Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),Validators.required]],
     password: ['', Validators.required],
-    reapeatPassword: ['', Validators.required],
-    terms: [false, Validators.required]
+    repeatPassword: ['', Validators.required],
+    terms: [true, Validators.required]
+  }, {
+    validators: this.samePasswords('password', 'repeatPassword')
   });
   
   
@@ -47,7 +49,38 @@ export class RegisterComponent {
     }
   }
 
+  unvalidPasswords(){
+    
+    const pass1 = this.registerForm.get('password').value;
+    const pass2 = this.registerForm.get('repeatPassword').value;
+    
+    
+
+    if ((pass1 !== pass2) && this.formSubmitted) {
+      return true
+    }else{
+      return false
+    }
+
+  }
   acceptTerms(){
     return !this.registerForm.get('terms').value && this.formSubmitted
   }
+
+  samePasswords(pass1name: string, pass2name: string){
+    // tenemos que retornar una función ya que los validadores son funciones(validators.required por ejemplo)
+    return (formGroup: FormGroup) =>{
+
+      const pass1Control = formGroup.get(pass1name);
+      const pass2Control = formGroup.get(pass2name);
+
+      if (pass1Control.value === pass2Control.value) {
+        pass2Control.setErrors(null)
+      }else{
+        pass2Control.setErrors({notTheSame: true})
+      }
+
+    }
+  }
+
 }
