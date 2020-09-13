eefe53622174371c4f10ac202e721c63192df6d0
Iván Sánchez <ivansancrodr@gmail.com>
2020-09-13 21:10:03 +0200

    añadido botón remember password en login, persistente medienate localhost

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UsuarioService } from '../../services/usuario.service';
@@ -10,31 +10,43 @@ import Swal from 'sweetalert2';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  
+  
   public loginForm = this.fb.group({
     email: [
-      'dummy@gmail.com',
+      localStorage.getItem('email') || '',
       [
         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
         Validators.required,
       ],
     ],
-    password: ['654321', Validators.required],
-    remember: [false],
+    password: ['', Validators.required],
+    remember: [localStorage.getItem('remember') || false],
   });
 
   constructor(
     private router: Router,
     private fb: FormBuilder,
     private userService: UsuarioService
-  ) {}
+  ) {
 
-  ngOnInit(): void {}
+    
+  }
+
+ 
 
   login() {
     this.userService.login(this.loginForm.value).subscribe(
       (res) => {
-        console.log(res);
+        if ( this.loginForm.get('remember').value) {
+          localStorage.setItem('email', this.loginForm.get('email').value);
+          localStorage.setItem('remember', this.loginForm.get('remember').value);
+        }else{
+          localStorage.removeItem('email');
+          localStorage.removeItem('remember');
+
+        }
       },
       (err) => {
         Swal.fire('Error', err.error.msg, 'error');
