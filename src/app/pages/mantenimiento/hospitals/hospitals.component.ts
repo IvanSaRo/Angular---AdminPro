34b67b0995d76396261a022a79616b0cd11e2896
Iván Sánchez <ivansancrodr@gmail.com>
2020-10-18 01:18:21 +0200

    implementado loading mediante ngIf, HTML con hospitales ok

--- src/app/pages/mantenimiento/hospitals/hospitals.component.ts
+++ src/app/pages/mantenimiento/hospitals/hospitals.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { HospitalService } from '../../../services/hospital.service';
+import { Hospital } from '../../../models/hospital.model';
 
 @Component({
   selector: 'app-hospitals',
   templateUrl: './hospitals.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HospitalsComponent implements OnInit {
+  public hospitals: Hospital[] = [];
+  public loading: boolean = true;
 
-  constructor(private hospitalService: HospitalService) { }
+  constructor(private hospitalService: HospitalService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {this.loadHospitals();}
 
-    this.hospitalService.getHospitals()
-        .subscribe( hospitals => console.log(hospitals)
-        
-        )
+  loadHospitals() {
+    this.loading = true;
+
+    this.hospitalService
+      .getHospitals()
+      .subscribe((hospitals) => {
+        this.hospitals = hospitals;
+        this.loading = false;
+      });
   }
-
-
 }
