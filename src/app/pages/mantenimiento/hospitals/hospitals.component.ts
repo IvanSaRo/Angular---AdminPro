1bde4957b827c8b3a1367fe2040fcce9154edd52
Iván Sánchez <ivansancrodr@gmail.com>
2020-10-19 00:38:37 +0200

    put hospital, delete y create hospital funcionales, añadido modal para carga de imágenes con el servicio y componente del modal

--- src/app/pages/mantenimiento/hospitals/hospitals.component.ts
+++ src/app/pages/mantenimiento/hospitals/hospitals.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { HospitalService } from '../../../services/hospital.service';
+import { delay } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+
 import { Hospital } from '../../../models/hospital.model';
+import { HospitalService } from '../../../services/hospital.service';
+import { ModalImgService } from '../../../services/modal-img.service';
 
 @Component({
   selector: 'app-hospitals',
@@ -10,19 +15,85 @@ import { Hospital } from '../../../models/hospital.model';
 export class HospitalsComponent implements OnInit {
   public hospitals: Hospital[] = [];
   public loading: boolean = true;
+  public imgSubs: Subscription;
 
-  constructor(private hospitalService: HospitalService) {}
+  constructor(
+    private hospitalService: HospitalService,
+    private modalImgService: ModalImgService
+  ) {}
 
-  ngOnInit(): void {this.loadHospitals();}
+  ngOnInit(): void {
+    this.loadHospitals();
+
+    this.imgSubs = this.modalImgService.newImg
+      .pipe(delay(100))
+      .subscribe((img) => this.loadHospitals());
+  }
 
   loadHospitals() {
     this.loading = true;
 
-    this.hospitalService
-      .getHospitals()
-      .subscribe((hospitals) => {
-        this.hospitals = hospitals;
-        this.loading = false;
+    this.hospitalService.getHospitals().subscribe((hospitals) => {
+      this.hospitals = hospitals;
+      this.loading = false;
+    });
+  }
+  guardarCambios(hospital: Hospital) {
+    const { name, _id } = hospital;
+
+    this.hospitalService.updateHospital(_id, name).subscribe((res) => {
+      Swal.fire('Actualizado', name, 'success'),
+        (err) => {
+          Swal.fire('Error', 'No se pudo actualizar', 'error');
+        };
+    });
+  }
+  eliminarHospital(hospital: Hospital) {
+    const { name, _id } = hospital;
+
+    Swal.fire({
+      title: 'Cuidado',
+      text: 'El borrado es permanente',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, bórralo',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.hospitalService.deleteHospital(_id).subscribe((res) => {
+          this.loadHospitals();
+          Swal.fire('Borrado', name, 'success'),
+            (err) => {
+              Swal.fire('Error', 'No se pudo borrar', 'error');
+            };
+        });
+      }
+    });
+  }
+
+  async abrirSwal() {
+    const { value } = await Swal.fire<string>({
+      title: 'Crear hospital',
+      text: 'Introduzca el nombre del hospital',
+      input: 'text',
+      inputPlaceholder: 'Nombre del hospital',
+      showCancelButton: true,
+    });
+    if (value.trim().length > 0) {
+      this.hospitalService.createHospital(value).subscribe((res) => {
+        this.loadHospitals();
+        Swal.fire('Hospital creado', value, 'success'),
+          (err) => {
+            Swal.fire('Error', 'No se pudo crear el hospital', 'error');
+          };
       });
+    } else {
+      return;
+    }
+  }
+
+  openModal(hospital: Hospital) {
+    this.modalImgService.openModal('hospitals', hospital._id, hospital.img);
   }
 }
--- src/app/services/hospital.service.ts
+++ src/app/services/hospital.service.ts
@@ -29,30 +29,26 @@ export class HospitalService {
 
     return this.http
       .get(url, this.headers)
-      .pipe(map((res: {ok: boolean, hospitals: Hospital[]}) => res.hospitals));
+      .pipe(
+        map((res: { ok: boolean; hospitals: Hospital[] }) => res.hospitals)
+      );
   }
-  
+
   createHospital(name: string) {
     const url = `${base_url}/hospitals`;
 
-    return this.http
-      .post(url, {name}, this.headers)
-      
+    return this.http.post(url, { name }, this.headers);
   }
-  
+
   updateHospital(_id: string, name: string) {
-    const url = `${base_url}/hospitals`;
+    const url = `${base_url}/hospitals/${_id}`;
 
-    return this.http
-      .put(url, {name}, this.headers)
-      
+    return this.http.put(url, { name }, this.headers);
   }
- 
-  deleteHospital(_id: string) {
-    const url = `${base_url}/hospitals`;
 
-    return this.http
-      .delete(url, this.headers)
-      
+  deleteHospital(_id: string) {
+    const url = `${base_url}/hospitals/${_id}`;
+
+    return this.http.delete(url, this.headers);
   }
 }
