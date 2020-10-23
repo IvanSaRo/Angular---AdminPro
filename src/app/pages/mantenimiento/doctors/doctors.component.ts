c717b3aaf5354e54f65df05398bfe9ec2311ffa6
Iván Sánchez <ivansancrodr@gmail.com>
2020-10-23 15:19:49 +0200

    añadida estructura html de creación doctor

--- src/app/pages/mantenimiento/doctors/doctors.component.ts
+++ src/app/pages/mantenimiento/doctors/doctors.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { delay } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
 
 import { Doctor } from '../../../models/doctor.model';
 
@@ -18,30 +19,27 @@ export class DoctorsComponent implements OnInit, OnDestroy {
     private doctorService: DoctorService,
     private searchsService: SearchsService,
     private modalImgService: ModalImgService
-
   ) {}
   public doctors: Doctor[] = [];
   public loading: boolean = true;
   public doctorsTemp: Doctor[] = [];
   public imgSubs: Subscription;
 
-
   ngOnInit(): void {
     this.loadDoctors();
 
     this.imgSubs = this.modalImgService.newImg
-    .pipe(delay(100))
-    .subscribe((img) => this.loadDoctors());
+      .pipe(delay(100))
+      .subscribe((img) => this.loadDoctors());
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.imgSubs.unsubscribe();
   }
 
   loadDoctors() {
     this.loading = true;
     this.doctorService.getHospitals().subscribe((doctors) => {
-      
       this.doctors = doctors;
       this.doctorsTemp = doctors;
       this.loading = false;
@@ -58,8 +56,31 @@ export class DoctorsComponent implements OnInit, OnDestroy {
       .subscribe((res) => (this.doctors = res));
   }
 
-  openModal(doctor: Doctor){
+  openModal(doctor: Doctor) {
     this.modalImgService.openModal('doctors', doctor._id, doctor.img);
-    
+  }
+
+  deleteDoctor(doctor: Doctor) {
+    const { name, _id } = doctor;
+
+    Swal.fire({
+      title: 'Cuidado',
+      text: 'El borrado es permanente',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, bórralo',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.doctorService.deleteDoctor(_id).subscribe((res) => {
+          this.loadDoctors();
+          Swal.fire('Borrado', name, 'success'),
+            (err) => {
+              Swal.fire('Error', 'No se pudo borrar', 'error');
+            };
+        });
+      }
+    });
   }
 }
